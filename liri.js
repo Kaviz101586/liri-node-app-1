78e7488407ad0f266892f640f4a103c6f7ef789f
boc4life <lheandrew@gmail.com>
2019-01-23 08:28:29 -0500

    random function done

--- liri.js
+++ liri.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+var fs = require("fs");
 
 var operator = process.argv[2];
 var operand = process.argv[3];
@@ -10,7 +12,7 @@ evaluate();
 
 function evaluate() {
     switch (operator) {
-        case "spotify-this":
+        case "spotify-this-song":
         runSpotify();
         break
         
@@ -28,17 +30,27 @@ function evaluate() {
 }
 
 function runSpotify() {
-
+    console.log("spotify")
 }
 
 function runBands() {
-
+    console.log("bands")
 }
 
 function runOMDB() {
-
+    console.log("omdb")
 }
 
 function runRandom() {
-    
+    fs.readFile("random.txt", "utf8", function (error, data){
+        if (error) {
+            console.log("error")
+        }
+        else {
+            dataArray = data.split(",");
+            operator = dataArray[0];
+            operand = dataArray[1];
+            evaluate();
+        }
+    })
 }
